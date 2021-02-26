39dbc1d6c29507fd7402aa3ac915d2a399701d2d
jeongminhong <azurestefan@gmail.com>
2021-02-26 22:46:30 +0900

    footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import Adventure from '../Adventure';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Home() {
   return (
     <>
       <Adventure />
       <Cards />
+      <Footer />
     </>
   );
 }
